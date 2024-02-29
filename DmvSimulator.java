2b8b91c5085315d6911e2b9a2ae97ed58d0d20d2
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2024-02-29 16:19:02 -0500

    Simulator now gives a number

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -1,5 +1,11 @@
+import java.util.Random;
+
 public class DmvSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to the Department of Motor Vehicles");
+        Random rng = new Random();
+        int number = rng.nextInt(100) + 1;
+        System.out.println(String.format("Here is your number: %d", number));
+        System.out.println("Please wait for your number to be called.");
     }
 }
