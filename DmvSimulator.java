f4f67a1a0aa9ce8e5708aac15efe3db9059826a2
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2024-03-02 09:32:53 -0500

    Changed range of numbers to 200

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -1,16 +1,18 @@
 import java.util.Random;
 
 public class DmvSimulator {
+
+    private static final int numbers = 200;
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Welcome to the Department of Motor Vehicles");
         Random rng = new Random();
-        int number = rng.nextInt(100) + 1;
-        System.out.println(String.format("Here is your number: %d", number));
+        int number = rng.nextInt(numbers) + 1;
+        System.out.printf("Here is your number: %d%n", number);
         System.out.println("Please wait for your number to be called.");
-        for (int num = 0; num < 100; num++) {
-            System.out.println(String.format("Number %d", ((num + number) % 100) + 1));
+        for (int num = 0; num < numbers; num++) {
+            System.out.printf("Number %d%n", ((num + number) % numbers) + 1);
             Thread.sleep(100);
         }
-        System.out.println("I\'m sorry, you don\'t have the required paperwork");
+        System.out.println("I'm sorry, you don't have the required paperwork");
     }
 }
